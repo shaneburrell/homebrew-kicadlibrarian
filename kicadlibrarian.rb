473ff85514a46c5f538c6e0defb69a6b8bd9b4dc
Shane Burrell <shane@shaneburrell.com>
2015-02-17 15:19:38 -0500

    change level

--- kicadlibrarian.rb
+++ kicadlibrarian.rb
@@ -14,7 +14,7 @@ class Kicadlibrarian < Formula
   depends_on "curl"
   depends_on "libharu"
 
-  patch do
+  patch :p0 do
       url "https://gist.githubusercontent.com/shaneburrell/33577564323246490089/raw/3477164e0d741eb1a88e3207bef066314d90600d/CMAKE.patch"
       sha1 "27a90f67fa506e0bf8db75cc09c3700cd0d03163"
   end
